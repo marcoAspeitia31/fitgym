c51e814c2203bd4b9d65c968c72c18a5feff6c71
Marco Aspeitia <maspeitia.31@gmail.com>
2020-04-05 22:01:13 -0500

    Header in index.php

--- page.php
+++ page.php
@@ -1,11 +1,6 @@
 <?php get_header(); ?>
 
 <?php while ( have_posts() ): the_post(); ?>
-    <h1>
-        <?php the_title(); ?>
-    </h1>>
+    <h1> <?php the_title(); ?> </h1>>
     <?php the_content(); ?>
-    Escrito por: <?php the_author(); ?>
-    Fecha: <?php the_date(); ?>
-
 <?php endwhile; ?>
