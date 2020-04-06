dc7b19a58e5fa056b3eeda9105ff2b435d66b143
Marco Aspeitia <maspeitia.31@gmail.com>
2020-04-05 21:45:19 -0500

    Header was created

--- page.php
+++ page.php
@@ -1,6 +1,11 @@
-<?php while(have_posts() ): the_post(  ); ?>
-    <h1><?php the_title(); ?></h1>>
+<?php 
+    while(have_posts() ): the_post(  );
+?>
+    <h1>
+        <?php the_title(); ?>
+    </h1>>
     <?php the_content(); ?>
     Escrito por: <?php the_author(); ?>
     Fecha: <?php the_date(); ?>
+
 <?php endwhile; ?>
