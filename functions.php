590f014628edaa27cbdee7e8988e9b5236cf75c6
Marco Aspeitia <maspeitia.31@gmail.com>
2020-04-06 00:01:58 -0500

    Habilitanto hoja de estilos CSS, functions.php

--- functions.php
+++ functions.php
@@ -7,4 +7,11 @@ function fitgym_menus(){
     ));      
 }
 
-add_action('init', 'fitgym_menus');
+add_action('init', 'fitgym_menus'); //hook para agregar funciones, init cuando garque la página, y fitgym es nuestro Text domain
+
+//Scripts y Styles  -------------> va a funcionar siempre para cargar hojas de estilo y archivos javascript
+function fitgym_scripts_styles(){
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
+}
+
+add_action('wp_enqueue_scripts', 'fitgym_scripts_styles');
--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
 </head>
 <body>
 
