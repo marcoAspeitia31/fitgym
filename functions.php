b1069fe4d7c123d11a8d5f2c7fac7fcb4f6c6a55
Marco Aspeitia <maspeitia.31@gmail.com>
2020-04-11 10:54:36 -0500

    Stylesheet normalize removed in functions.php

--- functions.php
+++ functions.php
@@ -11,12 +11,14 @@ add_action('init', 'fitgym_menus'); //hook para agregar funciones, init cuando g
 
 //Scripts y Styles  -------------> va a funcionar siempre para cargar hojas de estilo y archivos javascript
 function fitgym_scripts_styles() {
-wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+//wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
 
 wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap', array(), '1.0.0');
 
+echo get_stylesheet_uri();
+
     //hoja de estilos principal
-    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize, googleFont'), '1.0.0');
+    wp_enqueue_style('style', get_stylesheet_uri(), array('googleFont'), '1.0.0');
 }
 
 add_action('wp_enqueue_scripts', 'fitgym_scripts_styles');
