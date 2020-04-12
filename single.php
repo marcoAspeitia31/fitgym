e470c108b56ee84215e8dcedbe6bf2cf77657245
Marco Aspeitia <maspeitia.31@gmail.com>
2020-04-12 14:54:24 -0500

    Custom Post types fitgym template

--- single.php
+++ single.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
-<h1>post</h1>
 
 <?php while ( have_posts() ): the_post(); ?>
-    <h1> <?php the_title(); ?> </h1>>
+    <h1> <?php the_title(); ?> </h1>
     <?php the_content(); ?>
     Escrito por: <?php the_author(); ?>
     Fecha: <?php the_date(); ?>
--- template-parts/loop-paginas.php
+++ template-parts/loop-paginas.php
@@ -3,7 +3,17 @@
         <?php 
             if(has_post_thumbnail() ): //verifica si hay una imagen cargada
                 the_post_thumbnail('blog', array('class' => 'imagen-destacada')); //aquí se renderiza
-                endif;
+            endif;
+        ?>
+        <?php
+            //Verifica que el post agregaro sea de la clase fitgym_clases
+            if(get_post_type()==='fitgym_clases'){
+                $hora_inicio = get_field('hora_inicio');
+                $hora_fin = get_field('hora_fin');
+        ?>
+            <p><?php the_field('dias_clase'); ?> - <?php echo $hora_inicio . " a " .  $hora_fin; //advanced custom fields ?></p>
+        <?php
+            }
         ?>
     <?php the_content(); ?>
 <?php endwhile; ?>
