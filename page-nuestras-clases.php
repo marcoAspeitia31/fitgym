dbfac6ede0717ad87a2a56a64e7bee1191c7d33a
Marco Aspeitia <maspeitia.31@gmail.com>
2020-04-11 23:09:59 -0500

    Test

---
+++ page-nuestras-clases.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+    <main class="contenedor pagina seccion no-sidebar">
+        <div class="contenido-principal text-center">
+            <?php get_template_part('template-parts/loop-paginas')?> 
+        </div>
+    </main>
+
+<?php get_footer(); ?>
