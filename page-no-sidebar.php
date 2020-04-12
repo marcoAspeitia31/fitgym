074903b3b4912c0e1a57623854fdb217fc94a054
Marco Aspeitia <maspeitia.31@gmail.com>
2020-04-11 22:13:23 -0500

    Applying DRY method

--- page-no-sidebar.php
+++ page-no-sidebar.php
@@ -3,18 +3,12 @@
 /*
 * Template Name: Contenido centrado (No sidebars)
 */
+
+/* <?php get_template_part('template-parts/loop-paginas')?> aplicando el principio DRY (don't repeat yourself)*/
 get_header(); ?>
     <main class="contenedor pagina seccion no-sidebar">
         <div class="contenido-principal">
-            <?php while ( have_posts() ): the_post(); ?>
-                <h1 class="text-center texto-primario"> <?php the_title(); ?> </h1>
-                <?php 
-                    if(has_post_thumbnail() ): //verifica si hay una imagen cargada
-                        the_post_thumbnail('blog', array('class' => 'imagen-destacada')); //aquí se renderiza
-                    endif;
-                ?>
-                <?php the_content(); ?>
-            <?php endwhile; ?>
+            <?php get_template_part('template-parts/loop-paginas')?> 
         </div>
     </main>
 
