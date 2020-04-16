3ef291d1eb9a92222423bdcaff3c45e4d74ccf6b
Marco Aspeitia <maspeitia.31@gmail.com>
2020-04-15 23:04:27 -0500

    Starting CSS to the blog, class 79

--- home.php
+++ home.php
@@ -13,6 +13,9 @@
                             <a class="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
                                 <?php echo get_the_author_meta('display_name') ?>
                             </a>
+                        </p>
+                        <p class="meta">
+                            <span>Fecha:</span>
                             <?php the_time(get_option('date_format')); ?>
                         </p>
                     </div>
