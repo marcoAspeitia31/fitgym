b8ebde8f3cab2cdae214ef61380a5da77104f7c7
Marco Aspeitia <maspeitia.31@gmail.com>
2020-04-22 13:01:15 -0500

    Creating branch Inicio, classe 92

---
+++ front-page.php
@@ -0,0 +1,3 @@
+<?php get_header('front'); ?>
+
+<?php get_footer(); ?>
---
+++ header-front.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
+</head>
+<body>
+
+<header class="site-header">
+    <div class="contenedor">
+        <div class="barra-navegacion">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="logo alternativo">
+            </div>
+            <?php
+                $args = array(
+                    'theme_location' => 'menu-principal',
+                    'container' => 'nav',
+                    'container_class' => 'menu-principal'
+                );
+                wp_nav_menu($args);
+            ?>
+        </div>
+    </div>
+</header>
